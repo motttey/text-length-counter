40490d55afcfe137f7f4b1b11f06bc0642573512
motttey <motitago@gmail.com>
2022-07-29 21:47:57 +0900

    deal with react18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "bulma/css/bulma.css";
-import { render } from "react-dom";
 import App from "./App";
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-render(<App />, document.querySelector("#content"));
+const container = document.querySelector('#content');
+const root = createRoot(container);
+root.render(<App />);
